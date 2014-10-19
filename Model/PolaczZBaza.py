8afbd540c4947c44a5340a19e07a00f34eb14ecb
Fenton <fenton2020@gmail.com>
2014-10-19 21:50:39 +0200

    dodanie modulu PolaczZBaza

--- Model/PolaczZBaza.py
+++ Model/PolaczZBaza.py
@@ -1,51 +1,11 @@
 import MySQLdb
 import warnings
-# mysql -u root -p 
-# password ZooBaza
-
+from TabeleWBazie import TabeleWBazie
 warnings.filterwarnings('error', category=MySQLdb.Warning)
 
-# Open database connection
+# mysql -u root -p 
+# password ZooBaza
 mysql = MySQLdb.connect("localhost", "root", "ZooBaza", "Zoo")
-
-cursor = mysql.cursor()
-sqlStworzPozywienia = """CREATE TABLE IF NOT EXISTS POZYWIENIA (
-                        Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        Nazwa_Pozywienia  CHAR(20))"""
-
-sqlStworzZwierzeta = """CREATE TABLE IF NOT EXISTS ZWIERZETA (
-                        Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        Nazwa_Zwierzecia  CHAR(20))"""
-                      
-sqlLaczZwierzPoz = """CREATE TABLE IF NOT EXISTS LACZ_ZWIERZETA_POZYWIENIA (
-                      Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                      ZWIERZETA_Id INT NOT NULL,
-                      POZYWIENIA_Id INT NOT NULL,
-                      FOREIGN KEY ZWIERZETA_Id(ZWIERZETA_Id) REFERENCES ZWIERZETA(id),
-                      FOREIGN KEY POZYWIENIA_Id(POZYWIENIA_Id) REFERENCES POZYWIENIA(id))"""
-
-
-try:
-    cursor.execute(sqlStworzPozywienia)   
-except Warning, e:
-    print str(e)
-    
-try:
-    cursor.execute(sqlStworzZwierzeta)  
-except Warning, e:
-    print str(e)
-    
-try:
-    cursor.execute(sqlLaczZwierzPoz)  
-except Warning, e:
-    print str(e)
-    
-    
-try:
-    cursor.execute("""INSERT INTO POZYWIENIA(`Nazwa_Pozywienia`) VALUES('Pierogi')""")
-    cursor.execute("""INSERT INTO ZWIERZETA(`Nazwa_Zwierzecia`) VALUES('Jelen')""")
-except Warning, e:
-    print str(e)
-    
-# disconnect from server
+tabele = TabeleWBazie(mysql)
+tabele.stworzTabele()
 mysql.close()
